b8bd0445c602a7750a09baf904a555545725e8f5
Rahat <rahat@rahatcodes.com>
2023-12-12 13:00:49 -0500

    current

--- packages/client/src/mud/createSystemCalls.ts
+++ packages/client/src/mud/createSystemCalls.ts
@@ -1,44 +1,56 @@
-import { getComponentValue } from "@latticexyz/recs";
+import { Has, HasValue, getComponentValue, runQuery } from "@latticexyz/recs";
 import { ClientComponents } from "./createClientComponents";
 import { SetupNetworkResult } from "./setupNetwork";
 import { singletonEntity } from "@latticexyz/store-sync/recs";
-
+ 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
-
+ 
 export function createSystemCalls(
-  { worldContract, waitForTransaction }: SetupNetworkResult,
-  { SyncProgress }: ClientComponents
+  { playerEntity, worldContract, waitForTransaction }: SetupNetworkResult,
+  { MapConfig, Obstruction, Player, Position }: ClientComponents
 ) {
-  const moveTo = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+  const isObstructed = (x: number, y: number) => {
+    return runQuery([Has(Obstruction), HasValue(Position, { x, y })]).size > 0;
   };
-
+ 
   const moveBy = async (deltaX: number, deltaY: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+ 
+    const playerPosition = getComponentValue(Position, playerEntity);
+    if (!playerPosition) {
+      console.warn("cannot moveBy without a player position, not yet spawned?");
+      return;
+    }
+ 
+    const newX = playerPosition.x + deltaX;
+    const newY = playerPosition.y + deltaY;
+ 
+    if (isObstructed(newX, newY)) throw new Error("cannot go into an obstructed space");
+ 
+    const tx = await worldContract.write.moveBy([playerPosition.x, playerPosition.y, deltaX, deltaY]);
+    await waitForTransaction(tx);
   };
-
+ 
   const spawn = async (x: number, y: number) => {
-    // TODO
-    return null as any;
+    if (!playerEntity) {
+      throw new Error("no player");
+    }
+ 
+    const canSpawn = getComponentValue(Player, playerEntity)?.value !== true;
+    if (!canSpawn) {
+      throw new Error("already spawned");
+    }
+ 
+    if (isObstructed(x, y)) throw new Error("cannot go into an obstructed space");
+ 
+    const tx = await worldContract.write.spawn([x, y]);
+    await waitForTransaction(tx);
   };
-
-  const throwBall = async () => {
-    // TODO
-    return null as any;
-  };
-
-  const fleeEncounter = async () => {
-    // TODO
-    return null as any;
-  };
-
+ 
   return {
-    moveTo,
     moveBy,
     spawn,
-    throwBall,
-    fleeEncounter,
   };
 }
--- packages/contracts/src/systems/MapSystem.sol
+++ packages/contracts/src/systems/MapSystem.sol
@@ -1,10 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
-
+ 
 import { System } from "@latticexyz/world/src/System.sol";
-
+import { MapConfig, Movable, Obstruction, Player, Position } from "../codegen/index.sol";
+import { addressToEntityKey } from "../addressToEntityKey.sol";
+import { positionToEntityKey } from "../positionToEntityKey.sol";
+ 
 contract MapSystem is System {
-  function distance2(uint32 deltaX, uint32 deltaY) internal pure returns (uint32) {
-    return deltaX * deltaX + deltaY * deltaY;
+  function spawn(uint32 x, uint32 y) public {
+    bytes32 player = addressToEntityKey(address(_msgSender()));
+    require(!Player.get(player), "already spawned");
+ 
+    bytes32 position = positionToEntityKey(x, y);
+    require(!Obstruction.get(position), "this space is obstructed");
+ 
+    Player.set(player, true);
+    Position.set(player, x, y);
+    Movable.set(player, true);
+  }
+ 
+  function moveBy(uint32 clientX, uint32 clientY, int32 deltaX, int32 deltaY) public {
+    bytes32 player = addressToEntityKey(_msgSender());
+    require(Movable.get(player), "cannot move");
+ 
+    (uint32 fromX, uint32 fromY) = Position.get(player);
+    require(distance2(deltaX, deltaY) == 1, "can only move to adjacent spaces");
+    require(clientX == fromX && clientY == fromY, "client confused about location");
+ 
+    uint32 x = uint32(int32(fromX) + deltaX);
+    uint32 y = uint32(int32(fromY) + deltaY);
+ 
+    bytes32 position = positionToEntityKey(x, y);
+    require(!Obstruction.get(position), "this space is obstructed");
+ 
+    Position.set(player, x, y);
+  }
+ 
+  function distance2(int32 deltaX, int32 deltaY) internal pure returns (uint32) {
+    return uint32(deltaX * deltaX + deltaY * deltaY);
   }
 }
